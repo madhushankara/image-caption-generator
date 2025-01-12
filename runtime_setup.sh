33080f523b45c4d0be21e226431d1db280f45532
madhushankara <madhushankqra@gmail.com>
2025-01-12 13:56:20 +0000

    Update Procfile to run runtime_setup.sh before app

--- runtime_setup.sh
+++ runtime_setup.sh
@@ -3,6 +3,6 @@
 echo "Installing runtime dependencies..."
 
 # Adding the URL to download torch and torchaudio +cpu versions
-pip install sympy babel blis llvmlite spacy scipy numba matplotlib
+pip install sympy babel blis llvmlite spacy scipy numba
 
 echo "All dependencies installed successfully."
