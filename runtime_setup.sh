8c8f1c7a3e25400a2ccf5ff46fdb336454dd1f28
madhushankara <madhushankqra@gmail.com>
2025-01-12 09:02:46 +0000

    Updated runtime_setup.sh to fix torch installation issue

--- runtime_setup.sh
+++ runtime_setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 echo "Installing runtime dependencies..."
-pip install torch==2.1.0+cpu torchaudio==2.1.0+cpu transformers==4.47.1 TTS==0.22.0 --no-cache-dir
+
+# Adding the URL to download torch and torchaudio +cpu versions
+pip install \
+    --find-links https://download.pytorch.org/whl/torch_stable.html \
+    torch==2.1.0+cpu torchaudio==2.1.0+cpu transformers==4.47.1 TTS==0.22.0 --no-cache-dir
